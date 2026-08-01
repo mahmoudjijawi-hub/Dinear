3c6973b4b0e5d4fc0e0fb81769def1e76ec02f2b
Mahmoud jijawi <mahmoudjijawi@gmail.com>
2026-08-01 14:53:07 +0300

    Update settings.py

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get('DJANGO_DEBUG', 'True') == 'True'
 
-_allowed_hosts = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+_allowed_hosts = os.environ.get('DJANGO_ALLOWED_HOSTS','dinear-j7hu.onrender.com' ,'localhost,127.0.0.1').split(',')
 # السماح بنطاقات Cursor Cloud أثناء التطوير
 if DEBUG:
     _allowed_hosts.extend(['.cursorvm.com', '.agent.cvm.dev'])
